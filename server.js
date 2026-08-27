ddcc7be8481ef04e53e1dd566d2c99a6a6cc8fc4
SANKIOPS <sankiattire@protonmail.com>
2026-08-27 22:36:40 +0530

    Register credit card accounting routes

--- server.js
+++ server.js
@@ -2546,6 +2546,7 @@ app.use(require('./modules/fresh-procurement').router);
 app.use(require('./modules/casuals').router);
 app.use(require('./modules/size-tracker').router);
 app.use(require('./modules/expenses').router);
+app.use(require('./modules/credit-cards').router);
 app.use(require('./modules/telegram').router);
 app.use(require('./modules/salary').router);
 app.use(require('./modules/pl').router);
